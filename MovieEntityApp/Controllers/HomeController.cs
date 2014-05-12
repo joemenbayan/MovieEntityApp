c9efa5d8a2a092c4add191e8a0295931d01a448d
Joemen M Bayan <joemenbayan@gmail.com>
2014-05-12 23:22:42 +0800

    Push Initial Code

---
+++ MovieEntityApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieEntityApp.Models;
+
+namespace MovieEntityApp.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        MoviesDBEntities _db;
+
+        public HomeController()
+        {
+            _db = new MoviesDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            ViewData.Model = _db.MovieSet.ToList();
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Add(FormCollection form)
+        {
+            var movieToAdd = new Movie();
+
+            TryUpdateModel(movieToAdd, new string[] { "Title", "Director" }, form.ToValueProvider());
+
+            if (String.IsNullOrEmpty(movieToAdd.Title))
+                ModelState.AddModelError("Title", "Title is required");
+
+            if (String.IsNullOrEmpty(movieToAdd.Director))
+                ModelState.AddModelError("Director", "Director is required");
+
+            if (ModelState.IsValid)
+            {
+                _db.MovieSet.Add(movieToAdd);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(movieToAdd);
+        }
+
+    }
+}
---
+++ MovieEntityApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieEntityApp.MvcApplication" Language="C#" %>
